8796f7b9d38a853beab68fcf9ae7f4732d5115d8
AGoutieras <goutieras.anthony@gmail.com>
2025-11-25 13:56:35 +0000

    rewrote the function with structures and function pointers

--- _printf.c
+++ _printf.c
@@ -9,41 +9,43 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, count = 0;
 
 	va_start(args, format);
 
-	if (format == NULL)
-		return (-1);
+	int count = 0, index = 0, spec_index = 0;
 
-	while (format[i] != '\0')
+	specifier_t specs[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'d', print_int},
+		{'i', print_int},
+		{'\0', NULL}
+	};
+
+	for (index = 0; format[index]; index++)
 	{
-		if (format[i] == '%')
+		if (format[index] == '%')
 		{
-			i++;
-			if (format[i] == '\0')
-				return (-1);
+			index++;
+			if (format[index] == '\0')
+				break;
 
-			if (format[i] == 'c')
-				count = count + print_char(va_arg(args, int));
-			else if (format[i] == 's')
-				count = count + print_string(va_arg(args, char *));
-			else if (format[i] == 'd' || format[i] == 'i')
-				count = count + print_number(va_arg(args, int));
-			else if (format[i] == '%')
+			for (spec_index = 0; specs[spec_index].symbol; spec_index++)
 			{
-				print_char('%');
-				count++;
+				if (format[index] == specs[spec_index].symbol)
+				{
+					count = count + specs[spec_index].func(args);
+					break;
+				}
 			}
-			else
-				return (-1);
-			i++;
+			if (specs[spec_index].symbol == '\0')
+				count = count + write(1, &format[index], 1);
 		}
 		else
-		{	count = count + print_char(format[i]);
-			i++;
-		}
+			count = count + write(1, &format[index], 1);
 	}
+
 	va_end(args);
 	return (count);
 }
