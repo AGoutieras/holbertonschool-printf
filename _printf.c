7896c6beb187c5c5b2947a778fe6f3320349bb61
AGoutieras <goutieras.anthony@gmail.com>
2025-11-26 14:24:16 +0000

    added compatibility for unknown flags

--- _printf.c
+++ _printf.c
@@ -11,18 +11,12 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	int count = 0, index, spec_index;
-
 	specifier_t specs[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'d', print_int},
-		{'i', print_int},
-		{'\0', NULL}
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'d', print_int}, {'i', print_int}, {'\0', NULL}
 	};
 	if (format == NULL)
 		return (-1);
-
 	va_start(args, format);
 	for (index = 0; format[index] != '\0'; index++)
 	{
@@ -30,8 +24,10 @@ int _printf(const char *format, ...)
 		{
 			index++;
 			if (format[index] == '\0')
+			{
+				count = count + write(1, "%", 1);
 				break;
-
+			}
 			for (spec_index = 0; specs[spec_index].symbol; spec_index++)
 			{
 				if (format[index] == specs[spec_index].symbol)
@@ -41,7 +37,10 @@ int _printf(const char *format, ...)
 				}
 			}
 			if (specs[spec_index].symbol == '\0')
+			{
+				count = count + write(1, "%", 1);
 				count = count + write(1, &format[index], 1);
+			}
 		}
 		else
 			count = count + write(1, &format[index], 1);
