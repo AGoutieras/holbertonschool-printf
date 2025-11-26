312854790e5ac2308e1f7b5bc3d2cd5a3d8d1684
AGoutieras <goutieras.anthony@gmail.com>
2025-11-26 11:27:14 +0100

    Change print_percent to accept va_list argument

--- main.h
+++ main.h
@@ -23,7 +23,7 @@ int print_char(va_list args);
 int print_string(va_list args);
 
 /* function to print the percent symbol */
-int print_percent(void);
+int print_percent(va_list args);
 
 /* function to print integers */
 int print_int(va_list args);
